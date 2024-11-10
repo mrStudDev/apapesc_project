8cdd24cfc518a36051f6a5ad1249adb8237466cb
mrStudDev <marcosscfloripa@gmail.com>
2024-11-09 23:55:54 -0300

    Commit Nginx

--- app_home/models.py
+++ app_home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Home(models.Model):
+    nome = models.CharField(max_length=60)
+    meta_description = models.CharField(max_length=160)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return f"{self.nome}"
