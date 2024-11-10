88c0c5dd564a8445fbd5b5f2c17a12c45eabebc8
mrStudDev <marcosscfloripa@gmail.com>
2024-11-10 02:18:52 -0300

    Add Tailwind CSS configuration files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.html',
+    './app/**/*.html',
+    './app_home/**/*.html',
+    './static/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
